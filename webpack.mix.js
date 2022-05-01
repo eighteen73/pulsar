5963e8dd7a1cbb9b75b0cd710341e367f976d553
Ed Jeavons <ed@orphans.co.uk>
2022-05-01 15:03:44 +0100

    Mix BrowserSync config

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,6 @@
 const config = require('./config/mix.json')
 const mix = require('laravel-mix')
+const fs = require('fs')
 
 require('laravel-mix-clean')
 
@@ -19,33 +20,57 @@ if (mix.inProduction()) {
  * Stylesheets
  */
 
-config.entries.css.forEach(entry => {
-    mix.postCss(entry,'css')
-})
+config.entries.css.forEach(
+    entry => {
+        mix.postCss(entry, 'css')
+    }
+)
 
-mix.options({
-    processCssUrls: false,
-    postCss: [
-        require('autoprefixer'),
-        require('postcss-import'),
-        require('postcss-nested'),
-        require('tailwindcss')
-    ]
-})
+mix.options(
+    {
+        processCssUrls: false,
+        postCss: [
+            require('autoprefixer'),
+            require('postcss-import'),
+            require('postcss-nested'),
+            require('tailwindcss')
+        ]
+    }
+)
 
 /*
  * JavaScript
  */
 
-config.entries.js.forEach(entry => {
-    mix.js(entry,'js')
-})
+config.entries.js.forEach(
+    entry => {
+        mix.js(entry, 'js')
+    }
+)
 
-mix.autoload({
-    jquery: ['$', 'window.jQuery']
-})
+mix.autoload(
+    {
+        jquery: ['$', 'window.jQuery']
+    }
+)
 
 mix.extract()
+
+/*
+ * Browsersync (only if config/browsersync.json exists)
+ */
+
+if (fs.existsSync('./config/browsersync.json')) {
+    const browserSyncConfig = require('./config/browsersync.json')
+    browserSyncConfig.files = [
+        'dist/mix/dev/js/**/*.js',
+        'dist/mix/dev/css/**/*.css',
+        'parts/**/*.php',
+        'pattermns/**/*.php',
+        'templates/**/*.php',
+    ]
+    mix.browserSync(browserSyncConfig)
+}
 
 /*
  * Housekeeping
