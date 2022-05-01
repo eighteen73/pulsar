5394631890793b4a79e5d6bcacd68a27646d4b49
Ed Jeavons <ed@orphans.co.uk>
2022-05-01 13:51:48 +0100

    Formatting

--- includes/classes/Tools/Mix.php
+++ includes/classes/Tools/Mix.php
@@ -17,10 +17,9 @@ class Mix {
 	 */
 	protected static $mix = [];
 
-    protected static function build_mode()
-    {
-        return in_array( wp_get_environment_type(), [ 'development', 'local' ], true ) ? 'dev' : 'live';
-    }
+	protected static function build_mode() {
+		return in_array( wp_get_environment_type(), [ 'development', 'local' ], true ) ? 'dev' : 'live';
+	}
 
 	/**
 	 * Return the mix manifest.
@@ -31,9 +30,9 @@ class Mix {
 			return self::$mix;
 		}
 
-        $build_mode = self::build_mode();
-		$file      = get_parent_theme_file_path( "dist/mix/{$build_mode}/mix-manifest.json" );
-		self::$mix = (array) json_decode( file_get_contents( $file ), true );
+		$build_mode = self::build_mode();
+		$file       = get_parent_theme_file_path( "dist/mix/{$build_mode}/mix-manifest.json" );
+		self::$mix  = (array) json_decode( file_get_contents( $file ), true );
 
 		if ( is_child_theme() ) {
 			$child = get_theme_file_path() . 'dist/mix-manifest.json';
@@ -59,21 +58,21 @@ class Mix {
 	 */
 	public static function asset( $path ) {
 
-        // Get the Laravel Mix manifest.
-        $manifest = self::mix();
+		// Get the Laravel Mix manifest.
+		$manifest = self::mix();
 
-        // Make sure to trim any slashes from the front of the path.
-        $path = '/' . ltrim( $path, '/' );
+		// Make sure to trim any slashes from the front of the path.
+		$path = '/' . ltrim( $path, '/' );
 
-        if ( $manifest && isset( $manifest[ $path ] ) ) {
+		if ( $manifest && isset( $manifest[ $path ] ) ) {
 
-            // Use the dev build in development environments
-            $build_mode = self::build_mode();
-            $base_uri   = get_theme_file_uri( "dist/mix/{$build_mode}" );
+			// Use the dev build in development environments
+			$build_mode = self::build_mode();
+			$base_uri   = get_theme_file_uri( "dist/mix/{$build_mode}" );
 
-            return $base_uri . $manifest[ $path ];
-        }
+			return $base_uri . $manifest[ $path ];
+		}
 
-        return null;
+		return null;
 	}
 }
