e3dd6e7d1b1cce9b85e9b15de3793452da3b2d56
Brett Mason <brettsmason@gmail.com>
2022-06-29 19:53:44 +0100

    Disable core block patterns

--- includes/classes/Setup.php
+++ includes/classes/Setup.php
@@ -79,6 +79,14 @@ class Setup implements Bootable {
 
 		// Let core handle responsive embed wrappers.
 		add_theme_support( 'responsive-embeds' );
+
+		// Disable core block patterns.
+		remove_theme_support( 'core-block-patterns' );
+
+		// by adding the `theme.json` file block templates automatically get enabled.
+		// because the template editor will need additional QA and work to get right
+		// the default is to disable this feature.
+		remove_theme_support( 'block-templates' );
 	}
 
 	/**
