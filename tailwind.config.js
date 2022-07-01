6c0d84ee7df73da0928f017d9a1c9ca40848a210
Brett Mason <brett@orphans.co.uk>
2022-07-01 14:21:24 +0100

    config formatting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,19 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  content: require('fast-glob').sync([
-		'./**/*.php',
-		'./src/**/*.{css,js}',
-	]),
-  theme: {
-		container: ({ theme }) => ({
-      center: true,
-			padding: theme('spacing.6'),
-    }),
+	content: require('fast-glob').sync(['./**/*.php', './src/**/*.{css,js}']),
+	theme: {
+		container: {
+			center: true,
+		},
 		screens: {
-      'xs': '475px',
-      ...defaultTheme.screens,
-    },
-    extend: {},
-  },
-  plugins: [
+			xs: '475px',
+			...defaultTheme.screens,
+		},
+		extend: {},
+	},
+	plugins: [
 		require('@tailwindcss/typography'),
 		require('@tailwindcss/forms'),
 	],
-}
+};
