038d6791da94ed700c0479b227348f0f31697df7
Brett Mason <brett@orphans.co.uk>
2022-12-13 09:45:58 +0000

    Add default classes to the heading, list and paragraph blocks

--- src/js/block-filters/index.js
+++ src/js/block-filters/index.js
@@ -1,7 +1,10 @@
 /**
  * Entry point for block filters.
+ *
  * @see https://developer.wordpress.org/block-editor/reference-guides/filters/block-filters/
  */
 
 // Each block that needs filters to be applied, should have it's own file.
-// import './button'
+import './heading';
+import './list';
+import './paragraph';
---
+++ src/js/block-filters/paragraph.js
@@ -0,0 +1,33 @@
+import domReady from '@wordpress/dom-ready';
+import { addFilter } from '@wordpress/hooks';
+import { assign } from 'lodash';
+
+domReady(() => {
+	/**
+	 * Adds a default class to the paragraph block.
+	 * This may not be needed if the following is merged:
+	 *
+	 * @see https://github.com/WordPress/gutenberg/pull/42269
+	 *
+	 * @param {Object} settings The block settings
+	 * @param {string} name     The block name
+	 * @return {Object} The block settings
+	 */
+	function addParagraphBlockClassName(settings, name) {
+		if (name !== 'core/paragraph') {
+			return settings;
+		}
+
+		return assign({}, settings, {
+			supports: assign({}, settings.supports, {
+				className: true,
+			}),
+		});
+	}
+
+	addFilter(
+		'blocks.registerBlockType',
+		'pulsar/class-names/paragraph-block',
+		addParagraphBlockClassName
+	);
+});
