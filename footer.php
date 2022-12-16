342a2a19e282c6a2d107e21beadf9e263bf13193
Brett Mason <brett@orphans.co.uk>
2022-12-16 15:47:58 +0000

    Update lots of Sass structure

--- footer.php
+++ footer.php
@@ -7,9 +7,15 @@
 
 ?>
 
-		<footer id="colophon" class="site-footer">
-			<div class="container">
-				<!-- Footer content goes here -->
+		<footer id="colophon" class="site-footer container">
+			<div class="site-footer__inner">
+				<?php
+				printf(
+					esc_html__( 'Copyright &#169; %1$s %2$s', 'pulsar' ),
+					date_i18n( 'Y' ),
+					get_bloginfo( 'name' )
+				);
+				?>
 			</div>
 		</footer>
 	</div>
--- header.php
+++ header.php
@@ -18,11 +18,11 @@
 		<?php wp_body_open(); ?>
 
 		<div id="page" class="site">
-			<header id="masthead" class="site-header" x-data="menu()">
-				<div class="container">
+			<header id="masthead" class="site-header container" x-data="menu()">
+				<div class="site-header__inner">
 					<a class="screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'pulsar' ); ?></a>
 
-					<h1 class="site-header__title"><?php bloginfo( 'name' ); ?></h1>
+					<?php Pulsar\site_logo(); ?>
 
 					<?php get_template_part( 'parts/menus/primary' ); ?>
 				</div>
