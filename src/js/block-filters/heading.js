de348305eea1fc0268ecb84160deca9b63a7875d
Brett Mason <brett@orphans.co.uk>
2022-12-22 08:26:23 +0000

    Correct block filters syntax

--- src/js/block-filters/heading.js
+++ src/js/block-filters/heading.js
@@ -1,33 +1,30 @@
-import domReady from '@wordpress/dom-ready';
 import { addFilter } from '@wordpress/hooks';
 import { assign } from 'lodash';
 
-domReady(() => {
-	/**
-	 * Adds a default class to the heading block.
-	 * This may not be needed if the following is merged:
-	 *
-	 * @see https://github.com/WordPress/gutenberg/pull/42269
-	 *
-	 * @param {Object} settings The block settings
-	 * @param {string} name     The block name
-	 * @return {Object} The block settings
-	 */
-	function addHeadingBlockClassName(settings, name) {
-		if (name !== 'core/heading') {
-			return settings;
-		}
-
-		return assign({}, settings, {
-			supports: assign({}, settings.supports, {
-				className: true,
-			}),
-		});
+/**
+ * Adds a default class to the heading block.
+ * This may not be needed if the following is merged:
+ *
+ * @see https://github.com/WordPress/gutenberg/pull/42269
+ *
+ * @param {Object} settings The block settings
+ * @param {string} name     The block name
+ * @return {Object} The block settings
+ */
+function addHeadingBlockClassName(settings, name) {
+	if (name !== 'core/heading') {
+		return settings;
 	}
 
-	addFilter(
-		'blocks.registerBlockType',
-		'pulsar/class-names/heading-block',
-		addHeadingBlockClassName
-	);
-});
+	return assign({}, settings, {
+		supports: assign({}, settings.supports, {
+			className: true,
+		}),
+	});
+}
+
+addFilter(
+	'blocks.registerBlockType',
+	'pulsar/heading-block/class-names',
+	addHeadingBlockClassName
+);
