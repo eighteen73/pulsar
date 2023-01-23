f91a533db38f02738f2db685fe39ba13aa895e73
Brett Mason <brett@orphans.co.uk>
2023-01-23 13:36:02 +0000

    Disable Webpack from processing URLs

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,35 @@
 const defaultConfig = require('@wordpress/scripts/config/webpack.config');
 const { getWebpackEntryPoints } = require('@wordpress/scripts/utils/config');
 
+function regexEqual(x, y) {
+	return (
+		x instanceof RegExp &&
+		y instanceof RegExp &&
+		x.source === y.source &&
+		x.global === y.global &&
+		x.ignoreCase === y.ignoreCase &&
+		x.multiline === y.multiline
+	);
+}
+
+// We need to disable css-loader URL tampering in the Gutenberg webpack config so we can write relative URLs.
+// @see https://github.com/WordPress/gutenberg/blob/ba01e5d7851d6211d020687b5ae927f47ea4683e/packages/scripts/config/webpack.config.js#L51
+defaultConfig.module.rules = defaultConfig.module.rules.map((item) => {
+	if (
+		regexEqual(item.test, /\.css$/) ||
+		regexEqual(item.test, /\.(sc|sa)ss$/)
+	) {
+		item.use = item.use.map((useRule) => {
+			if (useRule.loader.match(/\/css-loader\//)) {
+				useRule.options.url = false;
+			}
+			return useRule;
+		});
+	}
+
+	return item;
+});
+
 module.exports = {
 	...defaultConfig,
 	stats: 'minimal',
