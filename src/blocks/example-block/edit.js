b7e7d895b73afa22f4c5e8463b6adca6f7f3c71b
Brett Mason <brett@orphans.co.uk>
2023-01-31 19:45:38 +0000

    Fix formatting

--- src/blocks/example-block/edit.js
+++ src/blocks/example-block/edit.js
@@ -10,43 +10,41 @@
  *
  * @see https://developer.wordpress.org/block-editor/reference-guides/richtext/
  */
- import { useBlockProps, RichText } from '@wordpress/block-editor';
- import { useSelect } from '@wordpress/data';
- import { __ } from '@wordpress/i18n';
+import { useBlockProps, RichText } from '@wordpress/block-editor';
+import { useSelect } from '@wordpress/data';
+import { __ } from '@wordpress/i18n';
 
- /**
-  * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
-  * Those files can contain any CSS code that gets applied to the editor.
-  *
-  * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
-  */
- import './editor.css';
+/**
+ * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
+ * Those files can contain any CSS code that gets applied to the editor.
+ *
+ * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
+ */
+import './editor.css';
 
- /**
-  * The edit function describes the structure of your block in the context of the
-  * editor. This represents what the editor will render when the block is used.
-  *
-  * @param {Object}   param0
-  * @param {Object}   param0.attributes
-  * @param {string}   param0.attributes.message
-  * @param {Function} param0.setAttributes
-  * @return {WPElement} Element to render.
-  */
- export default function Edit( { attributes: { message }, setAttributes } ) {
-	 const { title } = useSelect(
-		 ( select ) => select( 'core' ).getSite() ?? {}
-	 );
+/**
+ * The edit function describes the structure of your block in the context of the
+ * editor. This represents what the editor will render when the block is used.
+ *
+ * @param {Object}   param0
+ * @param {Object}   param0.attributes
+ * @param {string}   param0.attributes.message
+ * @param {Function} param0.setAttributes
+ * @return {WPElement} Element to render.
+ */
+export default function Edit({ attributes: { message }, setAttributes }) {
+	const { title } = useSelect((select) => select('core').getSite() ?? {});
 
-	 return (
-		 <p { ...useBlockProps() }>
-			 <RichText
-				 tagName="span"
-				 value={ message }
-				 onChange={ ( newMessage ) =>
-					 setAttributes( { message: newMessage } )
-				 }
-			 />
-			 <span> | { title ?? __( 'loading…', 'pulsar' ) }</span>
-		 </p>
-	 );
- }
+	return (
+		<p {...useBlockProps()}>
+			<RichText
+				tagName="span"
+				value={message}
+				onChange={(newMessage) =>
+					setAttributes({ message: newMessage })
+				}
+			/>
+			<span> | {title ?? __('loading…', 'pulsar')}</span>
+		</p>
+	);
+}
