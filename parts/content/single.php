9ff0545407541d926b9ab2c4a29e58a4e7c99e71
Brett Mason <brett@orphans.co.uk>
2023-07-07 09:58:04 +0100

    Remove traced of page title hide feature

--- parts/content/single.php
+++ parts/content/single.php
@@ -1,11 +1,9 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'entry' ); ?>>
-	<?php if ( Pulsar\display_post_title() ) : ?>
-		<header class="entry__header container">
-			<h1 class="entry__title">
-				<?php the_title(); ?>
-			</h1>
-		</header>
-	<?php endif; ?>
+	<header class="entry__header container">
+		<h1 class="entry__title">
+			<?php the_title(); ?>
+		</h1>
+	</header>
 
 	<div class="entry__content container block-vertical-spacing">
 		<?php the_content(); ?>
