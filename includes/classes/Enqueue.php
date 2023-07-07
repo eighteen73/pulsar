18c5d408ea9c763522a17bc8cc155f45679ebb98
Brett Mason <brett@orphans.co.uk>
2023-07-07 09:54:29 +0100

    Enqueue tidy up and allow loading individual block stylesheets

--- includes/classes/Enqueue.php
+++ includes/classes/Enqueue.php
@@ -9,6 +9,7 @@ namespace Pulsar;
 
 use Pulsar\Contracts\Bootable;
 use Pulsar\Tools\Asset;
+use Pulsar\Tools\Config;
 
 /**
  * Enqueue scripts, styles and fonts.
@@ -22,20 +23,22 @@ class Enqueue implements Bootable {
 	 * @return void
 	 */
 	public function boot() {
-		add_action( 'wp_enqueue_scripts', [ $this, 'styles' ], 10 );
-		add_action( 'wp_enqueue_scripts', [ $this, 'scripts' ], 10 );
-		add_action( 'admin_init', [ $this, 'editor_styles' ] );
+		add_action( 'wp_enqueue_scripts', [ $this, 'theme_styles' ], 10 );
+		add_action( 'wp_enqueue_scripts', [ $this, 'theme_scripts' ], 10 );
+		add_action( 'after_setup_theme', [ $this, 'block_styles' ] );
+		add_action( 'wp_enqueue_scripts', [ $this, 'disabled_block_styles' ], 999 );
+		add_action( 'after_setup_theme', [ $this, 'editor_styles' ] );
 		add_action( 'enqueue_block_editor_assets', [ $this, 'editor_scripts' ] );
 		add_action( 'admin_enqueue_scripts', [ $this, 'admin_styles' ], 10 );
 		add_action( 'admin_enqueue_scripts', [ $this, 'admin_scripts' ], 10 );
 	}
 
 	/**
-	 * CSS
+	 * Theme stylesheets.
 	 *
 	 * @return void
 	 */
-	public function styles() {
+	public function theme_styles() {
 
 		wp_enqueue_style(
 			'pulsar-app-styles',
@@ -46,7 +49,7 @@ class Enqueue implements Bootable {
 	}
 
 	/**
-	 * JavaScript
+	 * Theme scripts.
 	 *
 	 * @return void
 	 */
@@ -67,6 +70,81 @@ class Enqueue implements Bootable {
 	}
 
 	/**
+	 * Enqueues block-specific stylesheets so that they only load when the block
+	 * is in use. Block styles are stored under `/assets/css/blocks` are
+	 * automatically enqueued. Each file should be named
+	 * `{$block_namespace}-{$block_slug}.css`.
+	 *
+	 * @link  https://developer.wordpress.org/reference/functions/wp_enqueue_block_style/
+	 *
+	 * @return void
+	 */
+	public function block_styles() {
+
+		// Gets all the block stylesheets.
+		$files = glob( get_theme_file_path( 'dist/css/blocks/*.css' ) );
+
+		foreach ( $files as $file ) {
+
+			// Gets the filename without the path or extension.
+			$name = str_replace(
+				[
+					get_theme_file_path( 'dist/css/blocks/' ),
+					'.css',
+				],
+				'',
+				$file
+			);
+
+			// Sanitize the name to make sure it contains only
+			// characters allowed in a block type name.
+			$name = preg_replace( '/[^a-z0-9-]/', '', strtolower( $name ) );
+
+			// Get the position of the first hyphen.
+			$pos = strpos( $name, '-' );
+
+			// Bail if there is no hyphen.
+			if ( false === $pos ) {
+				continue;
+			}
+
+			// Converts the filename to its associated block name by
+			// replacing the first `-` with a `/`. Filenames must
+			// use `{namespace}-{slug}` for this to work.
+			$block = substr_replace( $name, '/', $pos, 1 );
+
+			// Register the block style.
+			wp_enqueue_block_style(
+				$block,
+				[
+					'handle'  => "pulsar-block-{$name}",
+					'src'     => get_theme_file_uri( "dist/css/blocks/{$name}.css" ),
+					'path'    => get_theme_file_path( "dist/css/blocks/{$name}.css" ),
+					'version' => Asset::attribute( $name, 'css/blocks', 'version' ),
+				]
+			);
+		}
+	}
+
+	/**
+	 * Disable individual block stylesheets.
+	 *
+	 * @return void
+	 */
+	public function disabled_block_styles() {
+
+		$blocks = Config::get( 'disabled-block-styles' );
+
+		if ( ! $blocks ) {
+			return;
+		}
+
+		foreach ( $blocks as $block ) {
+			wp_dequeue_style( "wp-block-{$block}" );
+		}
+	}
+
+	/**
 	 * Editor stylesheets.
 	 *
 	 * @return void
@@ -75,7 +153,7 @@ class Enqueue implements Bootable {
 
 		add_editor_style(
 			[
-				'dist/editor-styles.css',
+				'dist/css/editor-styles.css',
 			]
 		);
 	}
@@ -97,14 +175,14 @@ class Enqueue implements Bootable {
 	}
 
 	/**
-	 * Admin CSS
+	 * Admin stylesheets.
 	 *
 	 * @return void
 	 */
 	public function admin_styles() {}
 
 	/**
-	 * Admin JavaScript
+	 * Admin scripts.
 	 *
 	 * @return void
 	 */
