b27320cd6b6838cfc4a991d9fdb709d6146f5b64
Brett Mason <brett@orphans.co.uk>
2023-09-04 15:05:15 +0100

    Clearer example block pattern

--- patterns/example-pattern.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Title: Example Pattern
- * Slug: pulsar/example-pattern
- * Categories: pulsar
- *
- * @package Pulsar
- */
---
+++ patterns/example.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Title: Example
+ * Description: A description of your pattern
+ * Slug: pulsar/example
+ * Categories: text
+ * postTypes: page, post
+ * blockTypes:
+ * viewportWidth: 1280
+ * Keywords: example
+ * Inserter: true
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-patterns/#register_block_pattern
+ *
+ * @package Pulsar
+ */
