ac01e00536fa93b572bff23a3d645b74ef71b1f8
Brett Mason <brett@orphans.co.uk>
2023-09-04 15:07:45 +0100

    Add new PHP template parts directory

---
+++ template-parts/footer.php
@@ -0,0 +1,9 @@
+<div class="site-footer__inner has-global-padding is-layout-constrained">
+	<?php
+	printf(
+		esc_html__( 'Copyright &#169; %1$s %2$s', 'pulsar' ),
+		esc_html( date_i18n( 'Y' ) ),
+		esc_html( get_bloginfo( 'name' ) ),
+	);
+	?>
+</div>
---
+++ template-parts/header.php
@@ -0,0 +1,4 @@
+<div class="site-header__inner">
+	<?php Pulsar\site_logo(); ?>
+	<?php get_template_part( 'template-parts/menu', 'primary' ); ?>
+</div>
