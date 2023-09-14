7dbe31a1c527f6b4cfe1e5493be55d046f3ca50d
Brett Mason <brett@orphans.co.uk>
2023-09-14 11:51:52 +0100

    Progress with columns rework

--- src/js/block-filters/columns.js
+++ src/js/block-filters/columns.js
@@ -1,202 +1,153 @@
-import { createHigherOrderComponent } from '@wordpress/compose';
+// WordPress dependencies.
 import { InspectorControls } from '@wordpress/block-editor';
-import { PanelBody, ToggleControl } from '@wordpress/components';
+import {
+	ToggleControl,
+	// eslint-disable-next-line @wordpress/no-unsafe-wp-apis
+	__experimentalToggleGroupControl as ToggleGroupControl,
+	// eslint-disable-next-line @wordpress/no-unsafe-wp-apis
+	__experimentalToggleGroupControlOption as ToggleGroupControlOption,
+	PanelBody,
+} from '@wordpress/components';
 import { addFilter } from '@wordpress/hooks';
+import TokenList from '@wordpress/token-list';
+import { useEffect, useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
-import { assign, merge } from 'lodash';
-import { Fragment } from '@wordpress/element';
 
-/*
-    This modifies the block settings for the core/columns block
-    We enable support for className and define two additional state
-    attributes that will be used in the InspectorControls
-*/
-function modifyColumnsBlockSettings(settings, name) {
-	if (name !== 'core/columns') {
-		return settings;
-	}
-	return assign({}, settings, {
-		attributes: merge(settings.attributes, {
-			isStackedOnTablet: { type: 'boolean', default: false },
-			isStackedOnDesktop: { type: 'boolean', default: false },
-			isReversedWhenStacked: { type: 'boolean', default: false },
-		}),
-	});
-}
+const withMultiBreakpoint = (BlockEdit) => (props) => {
+	const { attributes, setAttributes } = props;
+	const { className } = attributes;
 
-/*
-    This adds a slider to the Inspector Controls for the core/columns block
-    and tracks the state / updates the block when changed
-*/
-const addInspectorControls = createHigherOrderComponent((BlockEdit) => {
-	return (props) => {
-		const {
-			attributes: {
-				isStackedOnMobile,
-				isStackedOnTablet,
-				isStackedOnDesktop,
-				isReversedWhenStacked,
-			},
-			setAttributes,
-			name,
-		} = props;
+	// Define our breakpoints.
+	const breakpoints = ['sm', 'md', 'lg', 'xl'];
 
-		if (name !== 'core/columns') {
-			return <BlockEdit {...props} />;
+	// Define the classes used for setting stacking.
+	const classes = {
+		reversed: 'is-reversed-when-stacked',
+		...breakpoints.reduce((acc, breakpoint) => {
+			acc[breakpoint] = `is-stacked-on-${breakpoint}`;
+			return acc;
+		}, {}),
+	};
+
+	// Set state for the active breakpoint.
+	const [activeBreakpoint, setActiveBreakpoint] = useState(false);
+
+	// Define useful helper text for each breakpoint.
+	const helpText = {
+		sm: __('Mobile screens.'),
+		md: __('Landscape mobiles and below.'),
+		lg: __('Tablets in portrait mode and below.'),
+		xl: __('Smaller laptops or tablets in landscape mode and below.'),
+	};
+
+	// Fetch the helper text for a breakpoint.
+	const getHelpText = (key) => helpText[key];
+
+	// Toggles a breakpoint class, and removes any other existing ones.
+	const toggleBreakpointClass = (toggleClass, enable) => {
+		const list = new TokenList(className);
+
+		// Remove all classes except the one being toggled
+		breakpoints.forEach((breakpoint) => {
+			if (breakpoint !== toggleClass) {
+				list.remove(classes[breakpoint]);
+			}
+		});
+
+		if (enable) {
+			list.add(toggleClass);
 		}
 
-		return (
-			<Fragment>
-				<BlockEdit {...props} />
-				<InspectorControls>
+		setAttributes({ className: list.value });
+	};
+
+	// Toggle the reversed class independently.
+	const toggleReversedClass = (enable) => {
+		const list = new TokenList(className);
+
+		if (enable) {
+			list.add(classes.reversed);
+		} else {
+			list.remove(classes.reversed);
+		}
+
+		setAttributes({ className: list.value });
+	};
+
+	// Remove all our custom classes.
+	// Used when stack on mobile is turned off.
+	const removeAllClasses = () => {
+		const list = new TokenList(className);
+
+		for (const size in classes) {
+			list.remove(classes[size]);
+		}
+
+		setAttributes({ className: list.value });
+	};
+
+	// Check if the columns have a particular class.
+	const hasClass = (className) => {
+		return attributes.className?.includes(className);
+	};
+
+	// Set the initial columns state, along with clearing classes when stacking is disabled.
+	useEffect(() => {
+		if (attributes.isStackedOnMobile) {
+			breakpoints.forEach((breakpoint) => {
+				if (hasClass(classes[breakpoint])) {
+					setActiveBreakpoint(breakpoint);
+					return;
+				} else {
+					setActiveBreakpoint('sm');
+					toggleBreakpointClass(classes.sm, true);
+				}
+			});
+		} else {
+			removeAllClasses();
+			setActiveBreakpoint(false);
+		}
+	}, [attributes.isStackedOnMobile]);
+
+	return 'core/columns' === props.name ? (
+		<>
+			<BlockEdit {...props} />
+			<InspectorControls>
+				{attributes.isStackedOnMobile && (
 					<PanelBody>
+						<ToggleGroupControl
+							label={__('Screen sizes up to')}
+							onChange={(value) => {
+								setActiveBreakpoint(value);
+								toggleBreakpointClass(classes[value], value);
+							}}
+							value={activeBreakpoint}
+							isBlock
+							help={getHelpText(activeBreakpoint)}
+						>
+							{breakpoints.map((breakpoint) => (
+								<ToggleGroupControlOption
+									key={breakpoint}
+									value={breakpoint}
+									label={__(breakpoint.toUpperCase())}
+								/>
+							))}
+						</ToggleGroupControl>
+
 						<ToggleControl
-							label={__('Stack on tablet')}
-							checked={isStackedOnTablet}
-							onChange={() =>
-								setAttributes({
-									isStackedOnTablet: !isStackedOnTablet,
-								})
-							}
+							label={__('Reversed when stacked')}
+							help={__('Reverse columns when stacked.')}
+							checked={hasClass(classes.reversed)}
+							onChange={(val) => {
+								toggleReversedClass(val);
+							}}
 						/>
 					</PanelBody>
-
-					<PanelBody>
-						<ToggleControl
-							label={__('Stack on desktop')}
-							checked={isStackedOnDesktop}
-							onChange={() =>
-								setAttributes({
-									isStackedOnDesktop: !isStackedOnDesktop,
-								})
-							}
-						/>
-					</PanelBody>
-
-					{(isStackedOnMobile ||
-						isStackedOnTablet ||
-						isStackedOnDesktop) && (
-						<PanelBody>
-							<ToggleControl
-								label={__('Reverse when stacked')}
-								help={__(
-									'Allows column order to be reversed when stacked. Useful for example if you have an image in the right column, but want it to be on top when stacked.'
-								)}
-								checked={isReversedWhenStacked}
-								onChange={() =>
-									setAttributes({
-										isReversedWhenStacked:
-											!isReversedWhenStacked,
-									})
-								}
-							/>
-						</PanelBody>
-					)}
-				</InspectorControls>
-			</Fragment>
-		);
-	};
-}, 'withInspectorControl');
-
-/**
- * Adds classes to the core/columns block in the editor.
- */
-const addEditorClasses = createHigherOrderComponent((BlockListBlock) => {
-	return (props) => {
-		const {
-			attributes: {
-				isStackedOnMobile,
-				isStackedOnTablet,
-				isStackedOnDesktop,
-				isReversedWhenStacked,
-			},
-			name,
-		} = props;
-
-		if (name !== 'core/columns') {
-			return <BlockListBlock {...props} />;
-		}
-
-		const classes = [];
-
-		if (isStackedOnMobile) {
-			classes.push(`is-stacked-on-mobile`);
-		}
-
-		if (isStackedOnTablet) {
-			classes.push(`is-stacked-on-tablet`);
-		}
-
-		if (isStackedOnDesktop) {
-			classes.push(`is-stacked-on-desktop`);
-		}
-
-		if (isReversedWhenStacked) {
-			classes.push('is-reversed-when-stacked');
-		}
-
-		return <BlockListBlock {...props} className={classes.join(' ')} />;
-	};
-}, 'withClientIdClassName');
-
-/**
- * Add classes to the frontend.
- */
-function addFrontendClasses(props, block, attributes) {
-	if (block.name !== 'core/columns') {
-		return props;
-	}
-
-	const classes = [props.className];
-
-	const {
-		isStackedOnMobile,
-		isStackedOnTablet,
-		isStackedOnDesktop,
-		isReversedWhenStacked,
-	} = attributes;
-
-	if (isStackedOnMobile) {
-		classes.push(`is-stacked-on-mobile`);
-	}
-
-	if (isStackedOnTablet) {
-		classes.push('is-stacked-on-tablet');
-	}
-
-	if (isStackedOnDesktop) {
-		classes.push('is-stacked-on-desktop');
-	}
-
-	if (isReversedWhenStacked) {
-		classes.push('is-reversed-when-stacked');
-	}
-
-	return assign({}, props, {
-		className: classes.join(' '),
-	});
-}
-
-addFilter(
-	'blocks.registerBlockType',
-	'pulsar/columns-block/block-settings',
-	modifyColumnsBlockSettings
-);
-
-addFilter(
-	'editor.BlockEdit',
-	'pulsar/columns-block/add-inspector-controls',
-	addInspectorControls
-);
-
-addFilter(
-	'editor.BlockListBlock',
-	'pulsar/columns-block/add-editor-classes',
-	addEditorClasses
-);
-
-addFilter(
-	'blocks.getSaveContent.extraProps',
-	'pulsar/columns-block/add-frontend-classes',
-	addFrontendClasses
-);
+				)}
+			</InspectorControls>
+		</>
+	) : (
+		<BlockEdit {...props} />
+	);
+};
+addFilter('editor.BlockEdit', 'pulsar/columns-block', withMultiBreakpoint);
