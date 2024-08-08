5ec57f63209fb7d747ac8f95176d652f1382e317
Brett Mason <brett@eighteen73.co.uk>
2024-08-08 14:03:55 +0100

    Better default 404

--- patterns/404.php
+++ patterns/404.php
@@ -12,30 +12,18 @@
 
 ?>
 
-<!-- wp:group {"tagName":"article","style":{"spacing":{"padding":{"top":"var:preset|spacing|5-xl","bottom":"var:preset|spacing|5-xl"}}},"layout":{"type":"constrained","contentSize":"800px"}} -->
-<article class="wp-block-group" style="padding-top:var(--wp--preset--spacing--5-xl);padding-bottom:var(--wp--preset--spacing--5-xl)">
-	<!-- wp:group {"tagName":"header","layout":{"type":"constrained"}} -->
-	<header class="wp-block-group">
-		<!-- wp:heading {"level":1} -->
-		<h1 class="wp-block-heading"><?php echo esc_html__( '404: Nothing Found', 'pulsar' ); ?></h1>
+<!-- wp:group {"tagName":"main","className":"site-main","layout":{"type":"constrained"}} -->
+<main class="wp-block-group site-main">
+	<!-- wp:group {"className":"","style":{"spacing":{"padding":{"top":"var:preset|spacing|4-xl","bottom":"var:preset|spacing|4-xl"}}},"layout":{"type":"constrained"}} -->
+	<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--4-xl);padding-bottom:var(--wp--preset--spacing--4-xl)">
+		<!-- wp:heading {"textAlign":"center","level":1,"className":""} -->
+		<h1 class="wp-block-heading has-text-align-center"><?php echo esc_html_e( '404: Nothing Found', 'pulsar' ); ?></h1>
 		<!-- /wp:heading -->
-	</header>
-	<!-- /wp:group -->
 
-	<!-- wp:group {"layout":{"type":"constrained"}} -->
-	<div class="wp-block-group">
-		<!-- wp:paragraph -->
-		<p><?php echo esc_html__( 'It looks like you stumbled upon a page that does not exist. Perhaps a search might help:', 'pulsar' ); ?></p>
+		<!-- wp:paragraph {"align":"center","className":""} -->
+		<p class="has-text-align-center"><?php echo esc_html_e( 'It looks like you stumbled upon a page that does not exist.', 'pulsar' ); ?></p>
 		<!-- /wp:paragraph -->
-
-		<!-- wp:search {
-			"label":"<?php echo esc_html__( 'Search', 'pulsar' ); ?>",
-			"showLabel":false,
-			"placeholder":"<?php echo esc_attr__( 'Enter search terms...', 'pulsar' ); ?>",
-			"buttonText":"<?php echo esc_html__( 'Search', 'pulsar' ); ?>",
-			"buttonPosition":"button-inside"
-		} /-->
 	</div>
 	<!-- /wp:group -->
-</article>
+</main>
 <!-- /wp:group -->
