1f1ca7d2dc689a194491eab749298049b8508478
Brett Mason <brett@eighteen73.co.uk>
2024-10-07 10:14:21 +0100

    Dedicated quantity inputs

--- includes/classes/ThirdParty/WooCommerce/SingleProduct.php
+++ includes/classes/ThirdParty/WooCommerce/SingleProduct.php
@@ -24,7 +24,62 @@ class SingleProduct implements Bootable {
 	 */
 	public function boot(): void {
 
-		// Remove the product description heading.
-		add_filter( 'woocommerce_product_description_heading', '__return_null' );
+		// Quantity buttons.
+		add_action( 'woocommerce_before_quantity_input_field', [ $this, 'quantity_minus_button' ], 10 );
+		add_action( 'woocommerce_after_quantity_input_field', [ $this, 'quantity_plus_button' ], 10 );
+		add_action( 'woocommerce_before_single_product', [ $this, 'quantity_buttons_script' ] );
+	}
+
+	/**
+	 * Quantity: Output minus button
+	 *
+	 * @return void
+	 */
+	public function quantity_minus_button(): void {
+		echo '<button class="quantity__button quantity__button--minus">-</button>';
+	}
+
+	/**
+	 * Quantity: Output plus button
+	 *
+	 * @return void
+	 */
+	public function quantity_plus_button() {
+		echo '<button class="quantity__button quantity__button--plus">+</button>';
+	}
+
+	/**
+	 * Quantity: buttons script
+	 *
+	 * @return void
+	 */
+	public function quantity_buttons_script() {
+		wc_enqueue_js(
+			"
+			document.querySelector('form.cart').addEventListener('click', function(event) {
+				if (event.target.classList.contains('quantity__button')) {
+					event.preventDefault();
+					var qty = event.target.closest('form.cart').querySelector('.qty');
+					var val = parseFloat(qty.value);
+					var max = parseFloat(qty.getAttribute('max'));
+					var min = parseFloat(qty.getAttribute('min'));
+					var step = parseFloat(qty.getAttribute('step'));
+					if (event.target.classList.contains('quantity__button--plus')) {
+						if (max && (max <= val)) {
+							qty.value = max;
+						} else {
+							qty.value = val + step;
+						}
+					} else {
+						if (min && (min >= val)) {
+							qty.value = min;
+						} else if (val > 1) {
+							qty.value = val - step;
+						}
+					}
+				}
+			});
+			"
+		);
 	}
 }
