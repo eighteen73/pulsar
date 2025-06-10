3eabcafa50f90fd7778ad8e9023351b2e762ec68
Brett Mason <brett@eighteen73.co.uk>
2025-06-10 10:36:59 +0100

    Update variation price and description via JS

--- includes/classes/ThirdParty/WooCommerce/SingleProduct.php
+++ includes/classes/ThirdParty/WooCommerce/SingleProduct.php
@@ -28,6 +28,8 @@ class SingleProduct implements Bootable {
 		add_action( 'woocommerce_before_quantity_input_field', [ $this, 'quantity_minus_button' ], 10 );
 		add_action( 'woocommerce_after_quantity_input_field', [ $this, 'quantity_plus_button' ], 10 );
 		add_action( 'woocommerce_before_single_product', [ $this, 'quantity_buttons_script' ] );
+		add_action( 'woocommerce_before_single_product', [ $this, 'variation_price_to_product_price' ] );
+		add_action( 'woocommerce_before_single_product', [ $this, 'variation_description_to_product_description' ] );
 	}
 
 	/**
@@ -90,6 +92,52 @@ class SingleProduct implements Bootable {
 				}
 			});
 			"
+		);
+	}
+
+	/**
+	 * Variation price to product price
+	 * This is to ensure that the price is updated when a variation is selected.
+	 *
+	 * @return void
+	 */
+	public function variation_price_to_product_price() {
+		global $product;
+		$price = $product->get_price_html();
+
+		wc_enqueue_js(
+			"
+			$(document).on('show_variation', 'form.cart', function( event, variation ) {
+				if(variation.price_html) $('[data-is-descendent-of-single-product-template=true] .wc-block-components-product-price').html(variation.price_html);
+				$('.woocommerce-variation-price').hide();
+			});
+			$(document).on('hide_variation', 'form.cart', function( event, variation ) {
+				$('[data-is-descendent-of-single-product-template=true] .wc-block-components-product-price').html('" . $price . "');
+			});
+			"
+		);
+	}
+
+	/**
+	 * Variation description to product description
+	 * This is to ensure that the description is updated when a variation is selected.
+	 *
+	 * @return void
+	 */
+	public function variation_description_to_product_description() {
+		global $product;
+		$description = $product->get_description();
+
+		wc_enqueue_js(
+			"
+			$(document).on('show_variation', 'form.cart', function( event, variation ) {
+				if(variation.variation_description) $('.wp-block-post-content').html(variation.variation_description);
+				$('.woocommerce-variation-description').hide();
+			});
+			$(document).on('hide_variation', 'form.cart', function( event, variation ) {
+				$('.wp-block-post-content').html('" . $description . "');
+			});
+		"
 		);
 	}
 }
